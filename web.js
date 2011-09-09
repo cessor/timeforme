1cd0efb2fbc93dbbf1e5fca2f7ff45cd27242135
Johannes Hofmeister <jhofmeister@equalexperts.com>
2011-09-09 21:48:38 +0200

    Added content type param. I am sorry, the code in web.js is so ugly, I apologize for what it looks like.

--- web.js
+++ web.js
@@ -4,20 +4,20 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	sendFile("index.html", response);
+	sendFile("index.html", response, "text/html");
 });
 
 app.get('/960/:file', function(request, response) {
-	sendFile('960/' + request.params.file, response);
+	sendFile('960/' + request.params.file, response, "text/css");
 });
 
-var sendFile = function(filename, response) {
+var sendFile = function(filename, response, contentType) {
 	fs.readFile(filename,function(err, data) {  
             if(err) {  
 				response.send("Apologies, dumb programmer exception. ", {'Content-Type' : "text/html"}, 500);  
                 return;  
             }  
-            response.send(data, {'Content-Type' : "text/html"}, 200);  
+            response.send(data, {'Content-Type' : contentType}, 200);  
         }); 
 };
 
