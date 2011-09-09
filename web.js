475412477281ded5e5c1e607b8838bab2c7e351d
Johannes Hofmeister <jhofmeister@equalexperts.com>
2011-09-09 21:38:35 +0200

    Added nodejs content delivery

--- web.js
+++ web.js
@@ -4,17 +4,22 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+	sendFile("index.html");
+});
 
-	var filename = "index.html";
-	
+app.get('/960/:file', function(request, response)) {
+	sendFile('960/' + request.params.file, response);
+};
+
+var sendFile = function(filename, response) {
 	fs.readFile(filename,function(err, data) {  
             if(err) {  
+				response.send("Apologies, dumb programmer exception. ", {'Content-Type' : "text/html"}, 500);  
                 return;  
             }  
-  
             response.send(data, {'Content-Type' : "text/html"}, 200);  
-        });  
-});
+        }); 
+};
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
