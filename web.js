52ff340c7a71c607b2036935e991275b52e9d97c
Johannes Hofmeister <jhofmeister@equalexperts.com>
2011-09-09 20:51:33 +0200

    Files based serve

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.send('Hello, World');
+
+	var filename = "hello.txt";
+	
+	fs.readFile(filename,function(err, data) {  
+            if(err) {  
+                response.sendHeader(500, {"Content-Type": "text/plain"});  
+                response.write(err + "\n");  
+                response.close();  
+                return;  
+            }  
+  
+            response.sendHeader(200);  
+            response.write(data, "text/html");  
+            response.close();  
+        });  
 });
 
 var port = process.env.PORT || 3000;
