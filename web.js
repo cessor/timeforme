9beb032c8d29181443b8bb961441cb7b7d1c941e
Johannes Hofmeister <jhofmeister@equalexperts.com>
2011-09-12 11:44:35 +0200

    Removed unnecessary configuration

--- web.js
+++ web.js
@@ -2,17 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
-var allowedTypes = {
-	"css" : "text/css",
-	"html" : "text/html"
-};
-
 app.set('views', __dirname + '/views');
-
-app.configure(function() {
-	app.use(express.logger());
-});
 
 app.get('/', function(request, response) {
 	res.render("index.html");
