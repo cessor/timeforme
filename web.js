11be9199ce105e714e5f5e30d23543e4984c411c
Johannes Hofmeister <jhofmeister@equalexperts.com>
2011-09-12 12:00:55 +0200

    I just want to serve a static file

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/');
 
 app.get('/', function(request, response) {
 	response.render("index.html");
