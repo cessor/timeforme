2a1bb4e4147ecb42c947396746d86d05401cbbb0
Johannes Hofmeister <jhofmeister@equalexperts.com>
2011-09-12 12:08:58 +0200

    Now that I added root to as a static content provider I should not really need the default / mapping

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express.createServer();
 app.set('views', __dirname + '/');
 
-app.get('/', function(request, response) {
-	response.render("index.jade");
-});
+aap.configure(function () {
+	app.use(express.logger());
+	app.use(express.staticProvider( __dirname + "/"));
+})
+
+//app.get('/', function(request, response) {
+//	response.render("index.jade");
+//});
 
 var port = process.env.PORT || 3000;
 app.listen(port);
