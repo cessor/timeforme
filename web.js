1a07eaf3cd342a54344ee167746325ad900708d7
Johannes Hofmeister <jhofmeister@equalexperts.com>
2011-09-12 11:47:14 +0200

    Typo. I hate this res, req style of variables.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.set('views', __dirname + '/views');
 
 app.get('/', function(request, response) {
-	res.render("index.html");
+	response.render("index.html");
 });
 
 var port = process.env.PORT || 3000;
